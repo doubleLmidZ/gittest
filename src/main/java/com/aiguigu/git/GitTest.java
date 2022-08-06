c673469e70855ca8a096a0b2e4c2097182f14b17
doubleLmidZ <lzl199707@163.com>
2022-08-06 16:25:48 +0800

    hot-fix commit 222

--- src/main/java/com/aiguigu/git/GitTest.java
+++ src/main/java/com/aiguigu/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello git22222222");
         System.out.println("hello git333333333333");
         System.out.println("hello git44444444444444444444");
+        System.out.println("hot-fix");
     }
 }
