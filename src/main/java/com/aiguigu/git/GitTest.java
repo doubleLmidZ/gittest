f219cffab99e2ba14a316a03db4f28f774efff99
doubleLmidZ <lzl199707@163.com>
2022-08-07 13:30:03 +0800

    push commit

--- src/main/java/com/aiguigu/git/GitTest.java
+++ src/main/java/com/aiguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git44444444444444444444");
         System.out.println("master");
         System.out.println("hot-fix");
+        System.out.println("push-test");
     }
 }
